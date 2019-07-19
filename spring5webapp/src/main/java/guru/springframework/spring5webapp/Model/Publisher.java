3e9424757c949a1737624c81de806de9b7e4ea05
Mahmud <mahmud.siraj@outlook.com>
2019-07-19 21:47:32 +0100

    Added Data repositories and initialised data

--- spring5webapp/src/main/java/guru/springframework/spring5webapp/Model/Publisher.java
+++ spring5webapp/src/main/java/guru/springframework/spring5webapp/Model/Publisher.java
@@ -11,8 +11,8 @@ public class Publisher {
     private String name;
     private String address;
 
-    @OneToOne(mappedBy = "publisher")
-    private Book book;
+//    @OneToOne(mappedBy = "publisher")
+//    private Book book;
 
     public Publisher() {
     }
@@ -22,19 +22,6 @@ public class Publisher {
         this.address = address;
     }
 
-    public Publisher(String name, String address, Book book) {
-        this.name = name;
-        this.address = address;
-        this.book = book;
-    }
-
-    public Book getBook() {
-        return book;
-    }
-
-    public void setBook(Book book) {
-        this.book = book;
-    }
 
     public Long getId() {
         return id;
@@ -61,12 +48,26 @@ public class Publisher {
     }
 
     @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Publisher publisher = (Publisher) o;
+
+        return id != null ? id.equals(publisher.id) : publisher.id == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+
+    @Override
     public String toString() {
         return "Publisher{" +
                 "id=" + id +
                 ", name='" + name + '\'' +
                 ", address='" + address + '\'' +
-                ", book=" + book +
                 '}';
     }
 }
